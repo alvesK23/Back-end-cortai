ea52407318cc7f4be8ae116b977f313211f3ca6b
matheus <89268454+mts-the-encoder@users.noreply.github.com>
2023-03-17 04:39:37 -0300

    user CRUD (#2)

--- back-end/src/main/java/sptech/school/backend/business/services/abstractions/IUserService.java
+++ back-end/src/main/java/sptech/school/backend/business/services/abstractions/IUserService.java
@@ -2,16 +2,15 @@ package sptech.school.backend.business.services.abstractions;
 
 import sptech.school.backend.comunication.request.RegisterRequest;
 import sptech.school.backend.comunication.response.UserResponse;
-import sptech.school.backend.entities.User;
 
 import javax.naming.NotContextException;
 import java.util.List;
+import java.util.Optional;
 
 public interface IUserService {
 
     List<UserResponse> findAll() throws NotContextException;
-    User save(RegisterRequest request);
-    UserResponse findById(Integer id);
-    User update(Integer id, RegisterRequest request);
-    void deleteById(Integer id);
+    Optional<UserResponse> findByFirstName(String firstName);
+    Optional<UserResponse> update(Integer id, RegisterRequest request);
+    void delete(Integer id);
 }
--- back-end/src/main/java/sptech/school/backend/repositories/IUserRepository.java
+++ back-end/src/main/java/sptech/school/backend/repositories/IUserRepository.java
@@ -1,11 +1,32 @@
 package sptech.school.backend.repositories;
 
+import java.awt.*;
+import java.lang.annotation.Native;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+
+import jakarta.persistence.NamedQuery;
+import jakarta.transaction.Transactional;
+import org.hibernate.annotations.NamedNativeQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import sptech.school.backend.entities.User;
+import sptech.school.backend.entities.enums.Role;
 
 public interface IUserRepository extends JpaRepository<User, Integer> {
 
   Optional<User> findByEmail(String email);
 
+  Optional<User> findByFirstName(String firstName);
+
+  List<User> findAllByRole(Role role);
+
+  @Modifying
+  @Transactional
+  @Query("DELETE FROM Token t  WHERE t.id =:id")
+  void deleteUser(Integer id);
 }
+
+
